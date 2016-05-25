74191000212f318974ab4f2eb8bf7dc4196c8b7c
Christopher Kiilu <cndeti@gmail.com>
2016-05-25 14:52:29 +0000

    Redirects depending on login

--- complaint_hub/complaints/views.py
+++ complaint_hub/complaints/views.py
@@ -16,7 +16,8 @@ def home(request):
 	context = {
 	}
 	return render(request, 'home.html', context)
-	
+
+@login_required()	
 def more_details(request):
 	staff = cache.get('staff')
 	
@@ -51,10 +52,16 @@ def more_details(request):
 	return render(request, 'user_profile.html', context)
 
 # Academic
+@login_required()
 def acad(request):
+	if StaffProfile.objects.filter(user=request.user).exists():
+		return redirect('staff_acad')
 	return render(request, 'acad.html')
 
+@login_required()
 def makeup(request):
+	if StaffProfile.objects.filter(user=request.user).exists():
+		return redirect('staff_makeup')
 	form = AcademicComplaintForm(data=request.POST)
 	user = StudentProfile.objects.get(user=request.user.id)
 	if form.is_valid():
@@ -71,7 +78,10 @@ def makeup(request):
 	}
 	return render(request, 'make_up_validation.html', context)
 
+@login_required()
 def portal(request):
+	if StaffProfile.objects.filter(user=request.user).exists():
+		return redirect('staff_portal')
 	form = AcademicComplaintForm(data=request.POST)
 	user = StudentProfile.objects.get(user=request.user.id)
 	if form.is_valid():
@@ -87,7 +97,10 @@ def portal(request):
 	}
 	return render(request, 'portal_validation.html', context)
 
+@login_required()
 def special(request):
+	if StaffProfile.objects.filter(user=request.user).exists():
+		return redirect('staff_special')
 	form = AcademicComplaintForm(data=request.POST)
 	user = StudentProfile.objects.get(user=request.user.id)
 	if form.is_valid():
@@ -103,7 +116,10 @@ def special(request):
 	}
 	return render(request, 'special_validation.html', context)
 
+@login_required()
 def verify(request):
+	if StaffProfile.objects.filter(user=request.user).exists():
+		return redirect('staff_verify')
 	form = AcademicComplaintForm(data=request.POST)
 	user = StudentProfile.objects.get(user=request.user.id)
 	if form.is_valid():
@@ -120,10 +136,16 @@ def verify(request):
 	return render(request, 'result_validation.html', context)
 
 # Adminstrative
+@login_required()
 def administrative(request):
+	if StaffProfile.objects.filter(user=request.user).exists():
+		return redirect('staff_administrative')
 	return render(request, 'admin.html')
 
+@login_required()
 def exeat(request):
+	if StaffProfile.objects.filter(user=request.user).exists():
+		return redirect('staff_exeat')
 	form = ExeatForm(data=request.POST)
 	user = StudentProfile.objects.get(user=request.user.id)
 	if form.is_valid():
@@ -138,7 +160,10 @@ def exeat(request):
 	}
 	return render(request, 'admin_exeat.html', context)
 
+@login_required()
 def work_study(request):
+	if StaffProfile.objects.filter(user=request.user).exists():
+		return redirect('staff_work_study')
 	form = WorkStudyForm(data=request.POST)
 	user = StudentProfile.objects.get(user=request.user.id)
 	if form.is_valid():
@@ -153,7 +178,10 @@ def work_study(request):
 	}
 	return render(request, 'admin_workstudy.html', context)
 
+@login_required()
 def ppd(request):
+	if StaffProfile.objects.filter(user=request.user).exists():
+		return redirect('staff_ppd')
 	form = PPDForm(data=request.POST)
 	user = StudentProfile.objects.get(user=request.user.id)
 	if form.is_valid():
@@ -168,7 +196,10 @@ def ppd(request):
 	}
 	return render(request, 'admin_ppd.html', context)
 
+@login_required()
 def admin_special(request):
+	if StaffProfile.objects.filter(user=request.user).exists():
+		return redirect('staff_admin_special')
 	form = SpecialAdmRequestForm(data=request.POST)
 	user = StudentProfile.objects.get(user=request.user.id)
 	if form.is_valid():
@@ -184,33 +215,57 @@ def admin_special(request):
 	return render(request, 'admin_special.html', context)
 
 # Questions
+@login_required()
 def faq(request):
 	return render(request, 'faq.html')
 
 # Staff pages
+@login_required()
 def staff_home(request):
+	if StudentProfile.objects.filter(user=request.user).exists():
+		return redirect('home')
 	return render(request, 'staff_home.html')
 
+@login_required()
 def staff_acad(request):
+	if StudentProfile.objects.filter(user=request.user).exists():
+		return redirect('acad')
 	return render(request, 'staff_acad.html')
 
+@login_required()
 def staff_makeup(request):
+	if StudentProfile.objects.filter(user=request.user).exists():
+		return redirect('makeup')
 	complaints = AcademicComplaint.objects.filter(request_type="Makeup").all()
+	if request.GET.get('id'):
+		complaint_id = request.GET.get('id')
+		c = AcademicComplaint.objects.get(id=complaint_id)
+		c.delete()
 	
 	context = {
 	'complaints': complaints,
 	}
 	return render(request, 'make_up_staff.html', context)
 
+@login_required()
 def staff_portal(request):
+	if StudentProfile.objects.filter(user=request.user).exists():
+		return redirect('portal')
 	complaints = AcademicComplaint.objects.filter(request_type="Portal").all()
+	if request.GET.get('id'):
+		complaint_id = request.GET.get('id')
+		c = AcademicComplaint.objects.get(id=complaint_id)
+		c.delete()
 	
 	context = {
 	'complaints': complaints,
 	}
 	return render(request, 'portal_staff.html', context)
 
+@login_required()
 def staff_special(request):
+	if StudentProfile.objects.filter(user=request.user).exists():
+		return redirect('special')
 	complaints = AcademicComplaint.objects.filter(request_type="Special").all()
 	
 	context = {
@@ -218,8 +273,15 @@ def staff_special(request):
 	}
 	return render(request, 'special_staff.html', context)
 
+@login_required()
 def staff_verify(request):
+	if StudentProfile.objects.filter(user=request.user).exists():
+		return redirect('verify')
 	complaints = AcademicComplaint.objects.filter(request_type="Result").all()
+	if request.GET.get('id'):
+		complaint_id = request.GET.get('id')
+		c = AcademicComplaint.objects.get(id=complaint_id)
+		c.delete()
 	
 	context = {
 	'complaints': complaints,
@@ -227,35 +289,65 @@ def staff_verify(request):
 	return render(request, 'result_staff.html', context)
 
 # Adminstrative
+@login_required()
 def staff_administrative(request):
+	if StudentProfile.objects.filter(user=request.user).exists():
+		return redirect('administrative')
 	return render(request, 'staff_admin.html')
 
+@login_required()
 def staff_exeat(request):
+	if StudentProfile.objects.filter(user=request.user).exists():
+		return redirect('exeat')
 	complaints = Exeat.objects.all()
-	
+	if request.GET.get('id'):
+		complaint_id = request.GET.get('id')
+		c = Exeat.objects.get(id=complaint_id)
+		c.delete()
 	context = {
 	'complaints': complaints,
 	}
 	return render(request, 'exeat_staff.html', context)
 
+@login_required()
 def staff_work_study(request):
+	if StudentProfile.objects.filter(user=request.user).exists():
+		return redirect('work_study')
 	complaints = WorkStudy.objects.all()
+	if request.GET.get('id'):
+		complaint_id = request.GET.get('id')
+		c = WorkStudy.objects.get(id=complaint_id)
+		c.delete()
 	
 	context = {
 	'complaints': complaints,
 	}
 	return render(request, 'work_study_staff.html', context)
 
+@login_required()
 def staff_ppd(request):
+	if StudentProfile.objects.filter(user=request.user).exists():
+		return redirect('ppd')
 	complaints = PPD.objects.all()
+	if request.GET.get('id'):
+		complaint_id = request.GET.get('id')
+		c = PPD.objects.get(id=complaint_id)
+		c.delete()
 	
 	context = {
 	'complaints': complaints,
 	}
 	return render(request, 'PPD_staff.html', context)
 
+@login_required()
 def staff_admin_special(request):
+	if StudentProfile.objects.filter(user=request.user).exists():
+		return redirect('admin_special')
 	complaints = SpecialAdmRequest.objects.all()
+	if request.GET.get('id'):
+		complaint_id = request.GET.get('id')
+		c = SpecialAdmRequest.objects.get(id=complaint_id)
+		c.delete()
 	
 	context = {
 	'complaints': complaints,
